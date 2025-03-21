32d2dc7d7ac15ae69b414c15adcf979dee098324
shahram <shahramsamar2010@gmail.com>
2025-03-21 16:02:53 +0400

    search class base

--- blog/views/blog_views.py
+++ blog/views/blog_views.py
@@ -5,14 +5,42 @@ from django.views.generic import (ListView,
                                   UpdateView,
                                   DeleteView,)
 from blog.models.blog_models import Post,Category
-
+from django.db.models import Q
 
 class BlogHomeView(ListView):
+    """
+    """
     model = Post
     template_name = 'blog.html'
     context_object_name = 'posts'
     success_url = '/'
     paginate_by = 4
+    ordering = ['-created_date']
+    
+    
+    def get_queryset(self):
+        """ 
+        get search item in title and content
+        """
+        queryset = super().get_queryset()
+        search_term = self.request.GET.get("s")
+        if search_term:
+            return queryset.filter(Q(title__icontains=search_term) | Q(content__icontains=search_term))
+        return queryset
+    
+    def get_context_data(self, **kwargs):
+        """ 
+       set context with queryset  and  return 
+        """
+        context = super().get_context_data(**kwargs)
+        context['search_term'] = self.request.GET.get('s','')
+        return context
+    
+    # def search(request,self):
+    #     if request.method == "GET":
+    #         if var :=request.GET.get("s"):
+    #             self.posts = self.posts.filter(content__=var)
+    #     return self.posts       
     
 # def BlogView(request):
 #     # template_name ='blog.html'
