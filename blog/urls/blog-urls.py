ade15dcaba114d5aa55b3b6de9aef4c54d066390
shahram <shahramsamar2010@gmail.com>
2025-03-22 00:18:39 +0400

    set category in sidebar

--- blog/urls/blog-urls.py
+++ blog/urls/blog-urls.py
@@ -4,6 +4,7 @@ from blog.views.blog_views import BlogHomeView,SingleBlogView
 
 urlpatterns = [
     path("",BlogHomeView.as_view(),name='blog'),
+    path('category/<str:category>/',BlogHomeView.as_view(), name='blog_category'),
     path('single/',SingleBlogView,name='single'),
 
     
--- blog/views/blog_views.py
+++ blog/views/blog_views.py
@@ -5,7 +5,8 @@ from django.views.generic import (ListView,
                                   UpdateView,
                                   DeleteView,)
 from blog.models.blog_models import Post,Category
-from django.db.models import Q
+from django.db.models import Q,Count
+
 
 class BlogHomeView(ListView):
     """
@@ -27,6 +28,12 @@ class BlogHomeView(ListView):
         search_term = self.request.GET.get("s")
         if search_term:
             return queryset.filter(Q(title__icontains=search_term) | Q(content__icontains=search_term))
+        
+        
+        category = self.kwargs.get('category')
+        if category:
+            return queryset.filter(category__name__iexact=category)
+        
         return queryset
     
     def get_context_data(self, **kwargs):
@@ -36,8 +43,26 @@ class BlogHomeView(ListView):
         context = super().get_context_data(**kwargs)
         context['search_term'] = self.request.GET.get('s','')
         context['latest_post'] = self.get_queryset().order_by('is_published')[:3]
+        # context['categories'] = Category.objects.filter(post__in=self.get_queryset())
+        context['categories'] = ( Post.objects.values('category__name').annotate(post_count=Count('id')).order_by('category__name').filter(post_count__gt=0) )
+
+        # for cat in context['categories']:
+
+        #     context['categories'][cat.name] = cat.post_count
+        #     context['count'] += 1
+        #         # context['cat_count'] = Category.objects.filter(category_count__gt=0).order_by('name')
+
+        # print(context['categories'].values())
+        # for k,v in context['categories']:
+        #     print(k,v)
         # print(context.keys())
         # print(context['latest_post'])
+        # print(context['paginator'])
+        # print(context['page_obj'])
+        # print(context['is_paginated'])
+        # print(context['object_list'])
+        # print(context['posts'])
+        # print(context['view'])
         return context
     
     
