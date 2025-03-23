75659fe540ad72e539aefbb44118cff4ab1257e7
shahram <shahramsamar2010@gmail.com>
2025-03-24 02:19:22 +0400

    Update admin.py

--- comment/admin.py
+++ comment/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from comment.models.comment_models import Comment
 
-# Register your models here.
+
+
+
+class CommentAdmin(admin.ModelAdmin):
+    date_hierarchy = 'created_date'
+    empty_value_display = '-empty-'
+    list_display = ('name','post','approved','created_date')
+    list_filter = ('post','approved')
+    search_fields = ['name','post']
+
+
+
+admin.site.register(Comment,CommentAdmin)
