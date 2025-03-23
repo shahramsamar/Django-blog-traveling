688fc04e24a504ae1d63d2eba637b3f2c754d523
shahram <shahramsamar2010@gmail.com>
2025-03-24 02:18:16 +0400

    Create newsletter.py

---
+++ blog/forms/newsletter.py
@@ -0,0 +1,10 @@
+from django import forms
+from blog.models.blog_models import NewsLetter
+
+class NewsLetterForm(forms.ModelForm):
+    
+    
+    class Meta:
+        model = NewsLetter
+        fields = '__all__'
+        
