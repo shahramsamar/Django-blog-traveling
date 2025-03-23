e1b8ed9e03e35c4662de68e640d0abde3d722e7c
shahram <shahramsamar2010@gmail.com>
2025-03-24 02:19:50 +0400

    Update comment_models.py

--- comment/models/comment_models.py
+++ comment/models/comment_models.py
@@ -1,3 +1,36 @@
-# from django.db import models
+from django.db import models
+from  blog.models.blog_models import Post
+from django.urls import reverse
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post,on_delete=models.CASCADE, related_name='comments')
+    author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
+
+    image = models.ImageField(upload_to='',default='user.jpeg')
+    name = models.CharField(max_length=50)
+    email = models.EmailField(max_length=100)
+    subject = models.CharField(max_length=255)
+    message = models.TextField()
+    approved = models.BooleanField(default=False)
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateField(auto_now=True)
+    
+    # def __str__(self):
+        # return self.name
+    
+    class Meta:
+        ordering = ['created_date']
+    
+    # def get_absolute_url(self):
+    #     return reverse("single", kwargs={"pk": self.pk})
+        
+    def __str__(self):
+        return f"Comment by {self.get_author_name()} on {self.post}"
+
+    def get_author_name(self):
+        return self.author.username if self.author else self.name
