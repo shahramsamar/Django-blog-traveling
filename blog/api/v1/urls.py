1017e6896ebbcb7385875b32c9abcfa536ab1f9b
shahramsamar <shahramsamar2010@gmail.com>
2025-09-30 09:54:22 +0330

    set router in url

--- blog/api/v1/urls.py
+++ blog/api/v1/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
 from blog.api.v1 import views
+from rest_framework.routers import DefaultRouter
 
 
-
+router = DefaultRouter()
+router.register("post", views.PostModelViewSet, basename='post')
 
 
 app_name='api-v1'
 
-urlpatterns = [
-
-]
+urlpatterns = router.urls
